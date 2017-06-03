94d005424d91caa61dc94d07caefa99bc885bb7e
Jodie Ly <jodie.ly@gmail.com>
2017-06-03 16:23:03 -0400

    added editor and button listeners

--- client/main.js
+++ client/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+// var loopback = require('loopback')
+// var User = loopback.User;
+// app.model(User);
+
+var options = {
+    theme: 'snow'
+};
+
+var editor = new Quill('#editor', options);
+
+editor.on('text-change', function() {
+    var text = editor.getText()
+    console.log(text);
+});
+
+$('button').on('click', function(){
+    console.log('hiii')
+})
